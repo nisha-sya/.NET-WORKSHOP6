efd2cebf51e04d6a0988b290442cca9c75d1bbeb
nisha-sya <netramayathapamagar93@gmail.com>
2025-12-04 10:15:55 +0545

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rectangle r = new Rectangle { Length = 5, Width = 3 };
+
+            Console.WriteLine($"Area = {r.GetArea()}");
+            Console.WriteLine($"Perimeter = {r.GetPerimeter()}");
+        }
+    }
+
+}
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Rectangle
+    {
+            public double Length { get; set; }
+            public double Width { get; set; }
+
+            // Expression-bodied properties
+            public double Area => Length * Width;
+
+            // Expression-bodied method
+            public double GetArea() => Length * Width;
+
+            // NEW: Perimeter (expression-bodied)
+            public double GetPerimeter() => 2 * (Length + Width);
+        }
+
+    }
+
